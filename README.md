0203a2f8e542d1dda3c251d1bb84bc3a9fe5dc20
l giesel <lgiesel543@yahoo.com>
2018-02-22 18:41:58 -0500

    Minor tweaks

---
+++ README.md
@@ -0,0 +1,3 @@
+SQL script is to create tables and initial data for an expense tracker mobile app. 
+
+Note: This script is part of a Meetup group to work on projects to help non-profit groups locally. The expense tracker app is to help the homeless keep track of daily expenses. 
--- SQLscripts/expense_tracker_tables.sql
+++ SQLscripts/expense_tracker_tables.sql
@@ -3,25 +3,25 @@
  CREATE DATABASE xp;
  USE xp;
 -- or 
--- individually drop tables in reverse order of FK constraINTs
+-- individually drop tables in reverse order of FK constraints
 --
 DROP TABLE IF EXISTS Expense;
 DROP TABLE IF EXISTS Category;
 
 CREATE TABLE Category
 (
-	ID					INT				PRIMARY KEY AUTO_INCREMENT,
-	CategoryName		VARCHAR(100) 	NOT NULL,
+	ID						INT	PRIMARY KEY AUTO_INCREMENT,
+	CategoryName	VARCHAR(100) 	NOT NULL,
 	CONSTRAINT ucat_name UNIQUE (CategoryName)
 );
 
 CREATE TABLE Expense
 (
-	ID					INT				PRIMARY KEY AUTO_INCREMENT,
-	Date				DATE			NOT NULL,
-	CategoryID			INT(12) 		NOT NULL,
-    Amount              DECIMAL(12, 2)  NOT NULL,
-	Expense_Desc		VARCHAR(75)		,
+	ID					  INT				PRIMARY KEY AUTO_INCREMENT,
+	Date				  DATE			NOT NULL,
+	CategoryID	  INT(12) 	NOT NULL,
+  Amount        DECIMAL(12, 2)  NOT NULL,
+	Expense_Desc	VARCHAR(75)		,
 	FOREIGN KEY (CategoryID) REFERENCES Category (ID)
 );
 
@@ -36,15 +36,4 @@ INSERT INTO category (ID, CategoryName) VALUES
 	(7, 'TRANSPORTATION'),
 	(8, 'MEDICINE'),
 	(9, 'CHILD SUPPORT/ALIMONY'),
-	(10, 'RENT');
-
---  (1, 'GENERAL'),
---  (2, 'GROCERIES'),
--- 	(3, 'BILLS'),
--- 	(4, 'COFFEE'),
---  (5, 'LOTTERY TICKETS'),
--- 	(6, 'CIGARETTES/TOBACCO'),
--- 	(7, 'PARKING TICKETS'),
--- 	(8, 'MEDICINE'),
--- 	(9, 'ALIMONY'),
--- 	(10, 'FOOD');
+	(10, 'RENT'); 
