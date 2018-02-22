7318ecf2492003089856a845848f75209bbdfd64
l giesel <lgiesel543@yahoo.com>
2018-02-21 23:25:04 -0500

    Initial commit

---
+++ SQLscripts/expense_tracker_tables.sql
@@ -0,0 +1,50 @@
+-- Can drop DB first, IF no other db users are using your DB
+ DROP DATABASE IF EXISTS xp;
+ CREATE DATABASE xp;
+ USE xp;
+-- or 
+-- individually drop tables in reverse order of FK constraINTs
+--
+DROP TABLE IF EXISTS Expense;
+DROP TABLE IF EXISTS Category;
+
+CREATE TABLE Category
+(
+	ID					INT				PRIMARY KEY AUTO_INCREMENT,
+	CategoryName		VARCHAR(100) 	NOT NULL,
+	CONSTRAINT ucat_name UNIQUE (CategoryName)
+);
+
+CREATE TABLE Expense
+(
+	ID					INT				PRIMARY KEY AUTO_INCREMENT,
+	Date				DATE			NOT NULL,
+	CategoryID			INT(12) 		NOT NULL,
+    Amount              DECIMAL(12, 2)  NOT NULL,
+	Expense_Desc		VARCHAR(75)		,
+	FOREIGN KEY (CategoryID) REFERENCES Category (ID)
+);
+
+-- Add or change categories as needed
+INSERT INTO category (ID, CategoryName) VALUES 
+	(1, 'GENERAL'),
+	(2, 'GROCERIES'),
+	(3, 'BILLS'),
+	(4, 'TAKE OUT/RESTAURANTS'),
+	(5, 'ENTERTAINMENT'),
+	(6, 'TOBACCO'),
+	(7, 'TRANSPORTATION'),
+	(8, 'MEDICINE'),
+	(9, 'CHILD SUPPORT/ALIMONY'),
+	(10, 'RENT');
+
+--  (1, 'GENERAL'),
+--  (2, 'GROCERIES'),
+-- 	(3, 'BILLS'),
+-- 	(4, 'COFFEE'),
+--  (5, 'LOTTERY TICKETS'),
+-- 	(6, 'CIGARETTES/TOBACCO'),
+-- 	(7, 'PARKING TICKETS'),
+-- 	(8, 'MEDICINE'),
+-- 	(9, 'ALIMONY'),
+-- 	(10, 'FOOD');
